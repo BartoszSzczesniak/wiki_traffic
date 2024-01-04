39ba000696b6cef8ebe2709c06611ce2d6a840fd
Bartosz <szczesniak.bartosz@gmail.com>
2024-01-04 13:40:50 +0100

    implemented Random Model

--- project_functions/baseline.py
+++ project_functions/baseline.py
@@ -9,27 +9,11 @@ class NaiveModel(tf.keras.Model):
         return tf.repeat(inputs[:, -1:, 0], self.predicted_window_size, axis=1)
 
 
-# # notes - attempts towards ARIMA
-# Might be used as a benchmark
+class RandomModel(tf.keras.Model):
+    def __init__(self, predicted_window_size):
+        super().__init__()
+        self.predicted_window_size = predicted_window_size
 
-# from darts.models import AutoARIMA
-# from darts import TimeSeries
-
-# i = 1
-
-# df_i = df_wiki_T.iloc[:, i]
-# df_i = df_i[df_i.index <= df_i[df_i != 0.0].index.max()]
-
-# ts_all = TimeSeries.from_series(df_i)
-
-# ts_train, ts_test = ts_all.split_before(0.9)
-
-# model = AutoARIMA()
-# model.fit(ts_train)
-
-# ts_pred = model.predict(len(ts_test))
-
-# df_i.plot(label="actual")
-# ts_pred.plot(label="forecast", lw=3)
-# plt.title(df_wiki_T.columns[i])
-
+    def call(self, inputs):
+        output_shape = (tf.shape(inputs)[0], self.predicted_window_size)
+        return tf.random.uniform(shape=output_shape)
